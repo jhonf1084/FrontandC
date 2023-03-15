4ca97eac99a282e617bb431ebce3fbee2bcba62e
Jhon F <jhon.vasquez@convertic.com.co>
2023-03-14 23:25:26 -0500

    primer commit

---
+++ FrontendC/src/app/home/fashion/fashion-two/fashion-two.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeSlider } from '../../../shared/data/slider';
+import { Product } from '../../../shared/classes/product';
+import { ProductService } from '../../../shared/services/product.service';
+
+@Component({
+  selector: 'app-fashion-two',
+  templateUrl: './fashion-two.component.html',
+  styleUrls: ['./fashion-two.component.scss']
+})
+export class FashionTwoComponent implements OnInit {
+
+  public themeLogo: string = 'assets/images/icon/logo-5.png'; // Change Logo
+
+  public products: Product[] = [];
+  public productCollections: any[] = [];
+  active;
+
+  constructor(public productService: ProductService) {
+    this.productService.getProducts.subscribe(response => {
+      this.products = response.filter(item => item.type == 'fashion');
+      // Get Product Collection
+      this.products.filter((item, i) => {
+        item.collection.filter((collection) => {
+          const index = this.productCollections.indexOf(collection);
+          if (index === -1) this.productCollections.push(collection);
+        })
+      })
+    });
+  }
+
+  public HomeSliderConfig: any = HomeSlider;
+
+  public sliders = [{
+    title: '',
+    subTitle: 'Hombre',
+    image: 'assets/images/slider/inicioHombre.jpg'
+  }]
+
+  // Collection banner
+  public collections1 = [];
+
+  public collections2 = [];
+
+  ngOnInit(): void {
+  }
+
+  // Product Tab collection
+  getCollectionProducts(collection) {
+    return this.products.filter((item) => {
+      if (item.collection.find(i => i === collection)) {
+        return item
+      }
+    })
+  }
+
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+# FrontendC
+Repositorio del frontend escrito en Angular para un Ecommerce
